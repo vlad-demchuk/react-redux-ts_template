8562005504dc1abe93167a25e5d55adee552eb10
Oleksandr Kryvosheiev <okryvosh@cisco.com>
2022-10-29 13:28:00 +0300

    created template

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App__header">
+        <h1 className="App__title">Feel free</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.scss";
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
